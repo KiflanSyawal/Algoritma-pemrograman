a108be231eb7316c5c4af3b3a315a5d12c47b1bd
KiflanSyawal <97747587+KiflanSyawal@users.noreply.github.com>
2022-09-14 10:34:03 +0700

    Add files via upload

---
+++ x dan y.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+
+public class Main {
+    public static void main(String[] args){
+        public static void main(String args[]) {
+            Scanner nilai = new Scanner(System.in);
+      
+          int x;
+          int y;
+      
+          System.out.print("Masukan nilai x : ");
+          x = nilai.nextInt();
+      
+          System.out.print("Masukan nilai y : ");
+          y = nilai.nextInt();
+      
+          int hasil = x + y;
+          System.out.println("Nilai dari x + y = "+hasil);
+}
+}
